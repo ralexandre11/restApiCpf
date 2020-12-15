ff711640a8a1e98ac400c8dcce98cabbc7b08881
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-12-15 14:27:49 -0300

    Initial Project

---
+++ src/main/java/com/ribeiro/restApiCpf/RestApiCpfApplication.java
@@ -0,0 +1,13 @@
+package com.ribeiro.restApiCpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCpfApplication.class, args);
+	}
+
+}
