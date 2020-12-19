58dd8229e3937b58e8997762d0939e640086938c
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-12-19 20:05:01 -0300

    Create PesonService and PersonServiceImpl

---
+++ src/main/java/com/ribeiro/restApiCpf/exception/MyRuleException.java
@@ -0,0 +1,9 @@
+package com.ribeiro.restApiCpf.exception;
+
+public class MyRuleException extends RuntimeException {
+	
+	public MyRuleException(String msg) {
+		super(msg);
+	}
+
+}
--- src/main/java/com/ribeiro/restApiCpf/service/PersonService.java
+++ src/main/java/com/ribeiro/restApiCpf/service/PersonService.java
@@ -1,5 +1,11 @@
 package com.ribeiro.restApiCpf.service;
 
+import com.ribeiro.restApiCpf.model.entity.Person;
+
 public interface PersonService {
+	
+	Person savePerson(Person person);
+	
+	void checkCpf(String cpf);
 
 }
