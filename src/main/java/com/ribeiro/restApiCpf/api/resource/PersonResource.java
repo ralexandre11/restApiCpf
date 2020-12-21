c3537d2f733494e5de1d76f05004ff4f6f960363
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-12-21 19:42:01 -0300

    Add controller component

---
+++ src/main/java/com/ribeiro/restApiCpf/api/resource/PersonResource.java
@@ -0,0 +1,89 @@
+package com.ribeiro.restApiCpf.api.resource;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ribeiro.restApiCpf.api.dto.PersonDTO;
+import com.ribeiro.restApiCpf.exception.MyRuleException;
+import com.ribeiro.restApiCpf.model.entity.Person;
+import com.ribeiro.restApiCpf.service.PersonService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/person")
+@RequiredArgsConstructor
+public class PersonResource {
+
+	private final PersonService service;
+	
+	@GetMapping
+	public ResponseEntity search(
+			@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "cpf", required = false) String cpf
+			) {
+		
+		Person personFilter = new Person();
+		personFilter.setName(name);
+		personFilter.setCpf(cpf);
+		List<Person> personList = service.searchPerson(personFilter);
+		return ResponseEntity.ok(personList);
+	}
+	
+
+	@PostMapping
+	public ResponseEntity save( @RequestBody PersonDTO dto ) {
+		try {
+			Person person = convert(dto);
+			Person personSaved = service.savePerson(person);
+			return new ResponseEntity(personSaved, HttpStatus.CREATED);
+		} catch (MyRuleException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@PutMapping("{id}")
+	public ResponseEntity update( @PathVariable("id") Integer id, @RequestBody PersonDTO dto) {
+		return service.getPersonByID(id).map(entity -> {
+			try {
+				Person person = convert(dto);
+				person.setId(entity.getId());
+				Person personSaved = service.updatePerson(person);
+				System.out.println(personSaved);
+				return ResponseEntity.ok(person);
+			} catch (Exception e) {
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+		}).orElseGet( () ->
+			new ResponseEntity("Registro inexistente!", HttpStatus.BAD_REQUEST ));
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity delete( @PathVariable("id") Integer id) {
+		return service.getPersonByID(id).map( entity -> {
+			service.deletePerson(entity);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}).orElseGet( () -> 
+			new ResponseEntity("Registro inexistente!", HttpStatus.BAD_REQUEST ));
+	}
+	
+	
+	private Person convert(PersonDTO dto) {
+		Person person = new Person();
+		person.setName(dto.getName());
+		person.setCpf(dto.getCpf());
+		
+		return person;
+	}
+}
--- src/main/java/com/ribeiro/restApiCpf/service/impl/PersonServiceImpl.java
+++ src/main/java/com/ribeiro/restApiCpf/service/impl/PersonServiceImpl.java
@@ -1,6 +1,12 @@
 package com.ribeiro.restApiCpf.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,17 +20,43 @@ public class PersonServiceImpl implements PersonService {
 	
 	private PersonRepository repository;
 	
-	@Autowired
+//	@Autowired
 	public PersonServiceImpl(PersonRepository repository) {
-		super();
+//		super();
 		this.repository = repository;
 	}
 
 	@Override
 	@Transactional
 	public Person savePerson(Person person) {
+		validateFields(person);
 		checkCpf(person.getCpf());
 		return repository.save(person);
+	}
+
+	@Override
+	@Transactional
+	public Person updatePerson(Person person) {
+		Objects.requireNonNull(person.getId());
+		validateFields(person);
+		return repository.save(person);
+	}
+
+	@Override
+	@Transactional
+	public void deletePerson(Person person) {
+		Objects.requireNonNull(person.getId());
+		repository.delete(person);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Person> searchPerson(Person personFilter) {
+		Example<Person> example = Example.of(personFilter, 
+				ExampleMatcher.matching()
+					.withIgnoreCase()
+					.withStringMatcher(StringMatcher.CONTAINING));
+		return repository.findAll(example);
 	}
 
 	@Override
@@ -34,6 +66,21 @@ public class PersonServiceImpl implements PersonService {
 			throw new MyRuleException("Já existe uma pessoal cadastrada com este CPF!");
 		}
 		
+	}
+	
+	public void validateFields(Person person) {
+		if(person.getName() == null || person.getName().trim().equals("")) {
+			throw new MyRuleException("Informe um Nome válido!");
+		}
+		if(person.getCpf() == null || person.getCpf().toString().length() != 11) {
+			throw new MyRuleException("Informe um CPF válido com 11 dígitos!");
+		}
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Person> getPersonByID(Integer id) {
+		return repository.findById(id);
 	}
 
 }
